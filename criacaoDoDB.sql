f3b0aa2cdbf356994f6a3512d7e84be1bff12765
José Elias <joseelias5589@gmail.com>
2024-10-31 08:45:30 -0300

    arquivos de configuração

---
+++ criacaoDoDB.sql
@@ -0,0 +1,38 @@
+CREATE DATABASE heather;
+USE heather;
+
+CREATE TABLE tb_login (
+    id_login INT PRIMARY KEY AUTO_INCREMENT,
+    ds_email VARCHAR(255),
+    ds_senha VARCHAR(255)
+);
+
+CREATE TABLE tb_servico (
+    id_servico INT PRIMARY KEY AUTO_INCREMENT,
+    titulo_tarefa VARCHAR(255),
+    nome_cliente VARCHAR(255),
+    tag_tarefa VARCHAR(255),
+    status_andamento VARCHAR(255),
+    data_entrega DATE
+);
+
+CREATE TABLE tb_portfolio (
+    id_portfolio INT PRIMARY KEY AUTO_INCREMENT,
+    imagem VARCHAR(255),
+    titulo VARCHAR(255),
+    descricao TEXT,
+    data_realizacao DATE
+);
+
+CREATE TABLE tb_mensagem (
+    id_mensagem INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(255),
+    email VARCHAR(255),
+    assunto VARCHAR(255),
+    corpo_mensagem TEXT
+);
+
+INSERT INTO tb_login (ds_email, ds_senha) VALUES (
+"login.com", "senha");
+
+SELECT * FROM tb_login;
