acc89b1f9a3036c84c4208822f23fc47ca9b3e1a
Edson Correia <edsoncorreia997@gmail.com>
2024-11-03 20:23:56 -0300

    ajustando controller portfolio

---
+++ heatherdesigner/src/controller/portfolioController.js
@@ -0,0 +1,97 @@
+import inserirPortfolio from "../service/portfolio/inserirPortfolio.js";
+import consultarPortfolio from "../service/portfolio/consultarPortfolio.js";
+import consultarPorIdPortfolio from "../service/portfolio/consultarPorIdPortfolio.js";
+import alterarPortfolio from "../service/portfolio/alterarPortfolio.js";
+import excluirPortfolio from "../service/portfolio/excluirPortfolio.js";
+
+
+import { Router } from "express";
+const endpoints = Router();
+
+
+// CREATE
+endpoints.post('/portfolio', async (req, resp) => {
+    try {
+        let portfolio = req.body
+
+        let id = await inserirPortfolio(portfolio);
+
+        resp.send({
+            novoId: id
+        })
+    } catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+
+
+})
+
+// READ
+
+endpoints.get('/portfolio', async (req, resp) => {
+    try {
+        let portfolios = await consultarPortfolio();
+        resp.send(portfolios);
+    } catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+
+
+})
+
+// READ BY ID
+endpoints.get('/portfolio/:id', async (req, resp) => {
+    try {
+        let idParaConsultar = Number(req.params.id);
+
+        let portfolio = await consultarPorIdPortfolio(idParaConsultar);
+        resp.send(portfolio);
+    }
+    catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+// UPDATE
+endpoints.put('/portfolio/:id', async (req, resp) => {
+    try {
+        let corpoParaAlterar = req.body;
+
+        let idParaAlterar = Number(req.params.id);
+
+        let portfolio = await alterarPortfolio(idParaAlterar, corpoParaAlterar);
+        resp.send({
+            idAlterar: idParaAlterar
+        })
+    }
+    catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+// DELETE
+endpoints.delete('/portfolio/:id', async (req, resp) => {
+    try {
+        let idParaDeletar = Number(req.params.id);
+        await excluirPortfolio(idParaDeletar);
+        resp.send({
+            idDeletado: idParaDeletar
+        })
+    }
+    catch (err) {
+        resp.status(400).send({
+            erro: err.message
+        })
+    }
+})
+
+
+export default endpoints;
--- heatherdesigner/src/repository/connection.js
+++ heatherdesigner/src/repository/connection.js
@@ -4,7 +4,7 @@ import mysql from 'mysql2/promise'
 let con = await mysql.createConnection({
     host: process.env.MYSQL_HOST,
     user: process.env.MYSQL_USER,
-    // password: process.env.MYSQL_PWD,
+    password: process.env.MYSQL_PWD,
     database: process.env.MYSQL_DB,
     typeCast: function (field, next) {
         if (field.type === 'TINY' && field.length === 1) {
